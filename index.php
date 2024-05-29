f86bb9a594f534a4b8faa9c8f7786eaea25a380e
SalutcestNI <tsenqo@gmail.com>
2024-05-29 10:16:54 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+$title = "SGC2B";
+$description = "";
+
+require_once 'config.php';
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<?php require_once 'includes/head.php'; ?>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php require_once 'includes/header.php'; ?>
+	<div class="container">
+		<div class="square-back">
+			<div class="sub-square-back">
+				<div class="sub-square-back-desc">
+					<p class="description51">Gestion et Communication</p>
+					<p class="gradient description52">Simple, Efficace</p>
+					<p class="gradient description52">Professionnel</p>
+					<p class="description53">L'endroit idéal pour gardez</p>
+					<p class="description53">un oeil sur vos enfants</p>
+				</div><span></span>
+				<div class="sub-square-back-img">
+					<span><img alt="Gardez un oeil sur votre enfant" height="600" src="/images/b4.jpg" width="600"></span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer class="footer">
+		<p>© 2024 SGC2B - Tous droits réservés</p>
+	</footer>
+	<script src="script.js">
+	</script>
+</body>
+</html>
